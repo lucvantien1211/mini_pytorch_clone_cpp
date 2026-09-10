dd65aa3f557da3664ec1ecb26e259265424f3c37
Nguyen Ngoc Lan <vishanty03@gmail.com>
2026-09-11 00:13:19 +0700

    Add tests for scalar ops on non-contiguous tensors

--- tests/math/test_scalar_ops.cpp
+++ tests/math/test_scalar_ops.cpp
@@ -14,10 +14,8 @@ TEST(ScalarOpsTest, TensorScalarAdditionProducesCorrectResult) {
     mt::Tensor d = b + a;
 
     // shape
-    EXPECT_EQ(c.shape()[0], 2);
-    EXPECT_EQ(c.shape()[1], 3);
-    EXPECT_EQ(d.shape()[0], 2);
-    EXPECT_EQ(d.shape()[1], 3);
+    EXPECT_EQ(c.shape(), std::vector<size_t>({2, 3}));
+    EXPECT_EQ(d.shape(), std::vector<size_t>({2, 3}));
 
     // value
     EXPECT_FLOAT_EQ(c.storage_at(0), 11.0f);
@@ -32,6 +30,34 @@ TEST(ScalarOpsTest, TensorScalarAdditionProducesCorrectResult) {
     EXPECT_FLOAT_EQ(d.storage_at(2), 13.0f);
     EXPECT_FLOAT_EQ(d.storage_at(3), 14.0f);
     EXPECT_FLOAT_EQ(d.storage_at(4), 15.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(5), 16.0f);
+}
+
+TEST(ScalarOpsTest, TensorScalarAdditionProducesCorrectResultOnNonContiguous) {
+    // Inout tensor a: [[1 4], [2 5], [3 6]]
+    mt::Tensor a = mt::arange(1.0f, 7.0f).reshape({2, 3}).transpose(0, 1);
+    float b = 10.0f;
+
+    mt::Tensor c = a + b;
+    mt::Tensor d = b + a;
+
+    // shape
+    EXPECT_EQ(c.shape(), std::vector<size_t>({3, 2}));
+    EXPECT_EQ(d.shape(), std::vector<size_t>({3, 2}));
+
+    // value
+    EXPECT_FLOAT_EQ(c.storage_at(0), 11.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(1), 14.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(2), 12.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(3), 15.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(4), 13.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(5), 16.0f);
+
+    EXPECT_FLOAT_EQ(d.storage_at(0), 11.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(1), 14.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(2), 12.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(3), 15.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(4), 13.0f);
     EXPECT_FLOAT_EQ(d.storage_at(5), 16.0f);
 }
 
@@ -56,10 +82,8 @@ TEST(ScalarOpsTest, TensorScalarSubtractionProducesCorrectResult) {
     mt::Tensor d = b - a;
 
     // shape
-    EXPECT_EQ(c.shape()[0], 2);
-    EXPECT_EQ(c.shape()[1], 3);
-    EXPECT_EQ(d.shape()[0], 2);
-    EXPECT_EQ(d.shape()[1], 3);
+    EXPECT_EQ(c.shape(), std::vector<size_t>({2, 3}));
+    EXPECT_EQ(d.shape(), std::vector<size_t>({2, 3}));
 
     // value
     EXPECT_FLOAT_EQ(c.storage_at(0), -9.0f);
@@ -74,6 +98,34 @@ TEST(ScalarOpsTest, TensorScalarSubtractionProducesCorrectResult) {
     EXPECT_FLOAT_EQ(d.storage_at(2), 7.0f);
     EXPECT_FLOAT_EQ(d.storage_at(3), 6.0f);
     EXPECT_FLOAT_EQ(d.storage_at(4), 5.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(5), 4.0f);
+}
+
+TEST(ScalarOpsTest, TensorScalarSubtractionProducesCorrectResultOnNonContiguous) {
+    // Inout tensor a: [[1 4], [2 5], [3 6]]
+    mt::Tensor a = mt::arange(1.0f, 7.0f).reshape({2, 3}).transpose(0, 1);
+    float b = 10.0f;
+
+    mt::Tensor c = a - b;
+    mt::Tensor d = b - a;
+
+    // shape
+    EXPECT_EQ(c.shape(), std::vector<size_t>({3, 2}));
+    EXPECT_EQ(d.shape(), std::vector<size_t>({3, 2}));
+
+    // value
+    EXPECT_FLOAT_EQ(c.storage_at(0), -9.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(1), -6.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(2), -8.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(3), -5.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(4), -7.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(5), -4.0f);
+
+    EXPECT_FLOAT_EQ(d.storage_at(0), 9.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(1), 6.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(2), 8.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(3), 5.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(4), 7.0f);
     EXPECT_FLOAT_EQ(d.storage_at(5), 4.0f);
 }
 
@@ -98,10 +150,8 @@ TEST(ScalarOpsTest, TensorScalarProductionProducesCorrectResult) {
     mt::Tensor d = b * a;
 
     // shape
-    EXPECT_EQ(c.shape()[0], 2);
-    EXPECT_EQ(c.shape()[1], 3);
-    EXPECT_EQ(d.shape()[0], 2);
-    EXPECT_EQ(d.shape()[1], 3);
+    EXPECT_EQ(c.shape(), std::vector<size_t>({2, 3}));
+    EXPECT_EQ(d.shape(), std::vector<size_t>({2, 3}));
 
     // value
     EXPECT_FLOAT_EQ(c.storage_at(0), 10.0f);
@@ -116,6 +166,34 @@ TEST(ScalarOpsTest, TensorScalarProductionProducesCorrectResult) {
     EXPECT_FLOAT_EQ(d.storage_at(2), 30.0f);
     EXPECT_FLOAT_EQ(d.storage_at(3), 40.0f);
     EXPECT_FLOAT_EQ(d.storage_at(4), 50.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(5), 60.0f);
+}
+
+TEST(ScalarOpsTest, TensorScalarProductionProducesCorrectResultOnNonContiguous) {
+    // Inout tensor a: [[1 4], [2 5], [3 6]]
+    mt::Tensor a = mt::arange(1.0f, 7.0f).reshape({2, 3}).transpose(0, 1);
+    float b = 10.0f;
+
+    mt::Tensor c = a * b;
+    mt::Tensor d = b * a;
+
+    // shape
+    EXPECT_EQ(c.shape(), std::vector<size_t>({3, 2}));
+    EXPECT_EQ(d.shape(), std::vector<size_t>({3, 2}));
+
+    // value
+    EXPECT_FLOAT_EQ(c.storage_at(0), 10.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(1), 40.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(2), 20.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(3), 50.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(4), 30.0f);
+    EXPECT_FLOAT_EQ(c.storage_at(5), 60.0f);
+
+    EXPECT_FLOAT_EQ(d.storage_at(0), 10.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(1), 40.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(2), 20.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(3), 50.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(4), 30.0f);
     EXPECT_FLOAT_EQ(d.storage_at(5), 60.0f);
 }
 
@@ -140,10 +218,8 @@ TEST(ScalarOpsTest, TensorScalarDivisionProducesCorrectResult) {
     mt::Tensor d = a / b;
 
     // shape
-    EXPECT_EQ(c.shape()[0], 2);
-    EXPECT_EQ(c.shape()[1], 3);
-    EXPECT_EQ(d.shape()[0], 2);
-    EXPECT_EQ(d.shape()[1], 3);
+    EXPECT_EQ(c.shape(), std::vector<size_t>({2, 3}));
+    EXPECT_EQ(d.shape(), std::vector<size_t>({2, 3}));
 
     // value
     EXPECT_FLOAT_EQ(c.storage_at(0), 10.0f);
@@ -159,6 +235,34 @@ TEST(ScalarOpsTest, TensorScalarDivisionProducesCorrectResult) {
     EXPECT_FLOAT_EQ(d.storage_at(3), 0.1f);
     EXPECT_FLOAT_EQ(d.storage_at(4), 0.1f);
     EXPECT_FLOAT_EQ(d.storage_at(5), 0.1f);
+}
+
+TEST(ScalarOpsTest, TensorScalarDivisionProducesCorrectResultOnNonContiguous) {
+    // Inout tensor a: [[1 4], [2 5], [3 6]]
+    mt::Tensor a = mt::arange(1.0f, 7.0f).reshape({2, 3}).transpose(0, 1);
+    float b = 10.0f;
+
+    mt::Tensor c = a / b;
+    mt::Tensor d = b / a;
+
+    // shape
+    EXPECT_EQ(c.shape(), std::vector<size_t>({3, 2}));
+    EXPECT_EQ(d.shape(), std::vector<size_t>({3, 2}));
+
+    // value
+    EXPECT_FLOAT_EQ(c.storage_at(0), 0.1f);
+    EXPECT_FLOAT_EQ(c.storage_at(1), 0.4f);
+    EXPECT_FLOAT_EQ(c.storage_at(2), 0.2f);
+    EXPECT_FLOAT_EQ(c.storage_at(3), 0.5f);
+    EXPECT_FLOAT_EQ(c.storage_at(4), 0.3f);
+    EXPECT_FLOAT_EQ(c.storage_at(5), 0.6f);
+
+    EXPECT_FLOAT_EQ(d.storage_at(0), 10.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(1), 2.5f);
+    EXPECT_FLOAT_EQ(d.storage_at(2), 5.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(3), 2.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(4), 10.0f / 3.0f);
+    EXPECT_FLOAT_EQ(d.storage_at(5), 10.0f / 6.0f);
 }
 
 TEST(ScalarOpsTest, TensorScalarDivisionDoesNotModifyInputs) {
